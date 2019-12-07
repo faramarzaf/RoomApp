d6a90dff38e7e9daada6bea8b31c603d8e163a77
FaramarzAf <faramarzafzali@gmail.com>
2019-12-07 12:35:21 +0330

    first push

---
+++ app/src/main/java/com/faraaf/tictacdev/roomapp/Constants.java
@@ -0,0 +1,8 @@
+package com.faraaf.tictacdev.roomapp;
+
+final public class Constants {
+    private Constants() {
+    }
+    public static final String TABLE_NAME_NOTE ="notes";
+    public static final String DB_NAME ="notesdb.db";
+}
